79ad51d2fa4a139a08d6431e2dbcd93e9074c49b
jp9000 <obs.jim@gmail.com>
2019-02-01 20:39:41 -0800

    Fix issues with cookies when renaming/copying/etc profiles [WIP]

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -1108,6 +1108,8 @@ static const double scaled_vals[] =
 	0.0
 };
 
+extern void CheckExistingCookieId();
+
 bool OBSBasic::InitBasicConfigDefaults()
 {
 	QList<QScreen*> screens = QGuiApplication::screens();
@@ -1312,6 +1314,8 @@ bool OBSBasic::InitBasicConfigDefaults()
 	config_set_default_double(basicConfig, "Audio", "MeterDecayRate",
 			VOLUME_METER_DECAY_FAST);
 	config_set_default_uint  (basicConfig, "Audio", "PeakMeterType", 0);
+
+	CheckExistingCookieId();
 
 	return true;
 }
